6b955ba2360f1e4ccb062b200622406e98f82a0c
Sneha Saj <snehasaj1999@gmail.com>
2020-08-18 13:01:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Avasarshala Helper',
+      title: 'Messenger',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
